90ef1426ca81b29f171c6ade3d9f1d49dc0ef94e
riya-0105 <riyasushma2019@gmail.com>
2024-07-20 18:51:40 +0530

    paths error

--- user/src/components/Dashboard.js
+++ user/src/components/Dashboard.js
@@ -2,18 +2,36 @@ import React, { useEffect, useState } from 'react';
 import './Dashboard.css';
 import ManageBot from './ManageBot';
 import Users from './Users';
+import axios from 'axios';
 import LogoutButton from './Logout';
 
 function Dashboard() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const userLocalStorage = localStorage.getItem('user');
 
-    if(userLocalStorage) {
-      setUser(userLocalStorage);
-    }
-  }, []);
+    const fetchUserData = async () => {
+      try {
+        const userLocalStorage = localStorage.getItem('user');
+        const userLocalData = localStorage.getItem('userData');
+
+        if (userLocalStorage) {
+          console.log("user local storage: ", userLocalStorage);
+          setUser(userLocalStorage);
+        }
+        if(userLocalData) {
+          console.log("user storage: ", userLocalData);
+          const parsedData = JSON.parse(userLocalData); 
+          await axios.post('https://ats-assignment-1.onrender.com/auth', parsedData);
+          console.log('User data posted successfully.');
+        }
+      } catch (error) {
+        console.error('Error posting user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, []); 
 
   return (
     <div className="Dashboard">
